37d4d3b04b0db358b9d1cec68a7a844afaab22ed
jlandesman <jlandesman@gmail.com>
2017-10-05 00:54:00 +0000

    updated mount_gc_data.sh

--- lib/mount_gc_data.sh
+++ lib/mount_gc_data.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-
+sudo rm -rf data
 mkdir data
 mkdir data/cbis_training
 mkdir data/mass_training_cropped
@@ -7,7 +7,7 @@ mkdir data/mass_training_full
 
 gcsfuse cbis-ddsm-calc-training-full-mammogram-images /home/jlandesman/data/cbis_training
 
-gcsfuse cbis-ddsm-mass-trainng-roi-and-cropped-images /home/jlandesman/data/mass_training_cropped
+gcsfuse cbis-ddsm-mass-training-roi-and-cropped-images /home/jlandesman/data/mass_training_cropped
 
 gcsfuse cbis-ddsm-mass-training-full-mammogram-images /home/jlandesman/data/mass_training_full
 
