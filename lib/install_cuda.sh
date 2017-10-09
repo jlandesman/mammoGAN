2cb16c9e2bba40c0e948f7570cc6c704be17d612
jlandesman <jlandesman@gpu-210.c.portfolio-181223.internal>
2017-10-09 13:13:17 +0000

    uploading cuda install script

---
+++ lib/install_cuda.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt-get install python-pip python-dev
+sudo -H pip install --upgrade pip
+
+wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
+
+sudo dpkg -i cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
+sudo apt-get update
+sudo apt-get install cuda
+
+echo "export CUDA_HOME=/usr/local/cuda-8.0" >> ~/.profile
+echo "export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64" >> ~/.profile
+
+echo "export PATH=/usr/local/cuda-8.0/bin:$PATH" >> ~/.profile
+source ~/.profile
+
+
