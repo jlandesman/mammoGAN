626f1c509f293eaed3dc8660867739a3b11669fd
jlandesman <jlandesman@users.noreply.github.com>
2017-10-09 16:41:25 -0400

    fixed typo 'trainnig --> training"

--- lib/mount_gc_data.sh
+++ lib/mount_gc_data.sh
@@ -17,14 +17,14 @@ mkdir data/mias_mini
 chmod -R 777 data
 
 gcsfuse cbis-ddsm-calc-training-full-mammogram-images /home/jlandesman/data/training/calc_full
-gcsfuse cbis-ddsm-calc-training-roi-and-cropped-images /home/jlandesman/data/trainnig/calc_cropped
+gcsfuse cbis-ddsm-calc-training-roi-and-cropped-images /home/jlandesman/data/training/calc_cropped
 gcsfuse cbis-ddsm-mass-training-roi-and-cropped-images /home/jlandesman/data/training/mass_cropped
 gcsfuse cbis-ddsm-mass-training-full-mammogram-images /home/jlandesman/data/training/mass_full
 
-gcsfuse cbis-ddsm-calc-test-full-mammogram-images /home/jlandesman/data/training/calc_full
-gcsfuse cbis-ddsm-calc-test-roi-and-cropped-images /home/jlandesman/data/trainnig/calc_cropped
-gcsfuse cbis-ddsm-mass-test-roi-and-cropped-images /home/jlandesman/data/training/mass_cropped
-gcsfuse cbis-ddsm-mass-test-full-mammogram-images /home/jlandesman/data/training/mass_full
+gcsfuse cbis-ddsm-calc-test-full-mammogram-images /home/jlandesman/data/test/calc_full
+gcsfuse cbis-ddsm-calc-test-roi-and-cropped-images /home/jlandesman/data/test/calc_cropped
+gcsfuse cbis-ddsm-mass-test-roi-and-cropped-images /home/jlandesman/data/test/mass_cropped
+gcsfuse cbis-ddsm-mass-test-full-mammogram-images /home/jlandesman/data/test/mass_full
 
 gcsfuse mias-mini-tiff /home/jlandesman/data/mias_mini
 
