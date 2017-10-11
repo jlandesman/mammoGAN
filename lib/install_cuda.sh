17744ef072c4e43e411268d3f584299ddc1f1188
jlandesman <jlandesman@users.noreply.github.com>
2017-10-11 19:08:59 -0400

    added install gcc

--- lib/install_cuda.sh
+++ lib/install_cuda.sh
@@ -3,6 +3,8 @@
 sudo apt-get install python3-pip python3-dev
 sudo -H pip3 install --upgrade pip
 
+sudo apt-get install gcc
+
 wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
 
 sudo dpkg -i cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
