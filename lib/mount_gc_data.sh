9ad9cca41dce17ee52048b034296acff78d6d509
jlandesman <jlandesman@users.noreply.github.com>
2017-10-11 20:01:03 -0400

    changed dir structure to "train" from "training"

--- lib/mount_gc_data.sh
+++ lib/mount_gc_data.sh
@@ -1,11 +1,11 @@
 #!bin/bash
 sudo rm -rf data/
 mkdir data
-mkdir data/training
-mkdir data/training/mass_cropped
-mkdir data/training/mass_full
-mkdir data/training/calc_full
-mkdir data/training/calc_cropped
+mkdir data/train
+mkdir data/train/mass_cropped
+mkdir data/train/mass_full
+mkdir data/train/calc_full
+mkdir data/train/calc_cropped
 
 mkdir data/test
 mkdir data/test/mass_cropped
@@ -16,10 +16,10 @@ mkdir data/test/calc_cropped
 mkdir data/mias_mini
 chmod -R 777 data
 
-gcsfuse cbis-ddsm-calc-training-full-mammogram-images /home/jlandesman/data/training/calc_full
-gcsfuse cbis-ddsm-calc-training-roi-and-cropped-images /home/jlandesman/data/training/calc_cropped
-gcsfuse cbis-ddsm-mass-training-roi-and-cropped-images /home/jlandesman/data/training/mass_cropped
-gcsfuse cbis-ddsm-mass-training-full-mammogram-images /home/jlandesman/data/training/mass_full
+gcsfuse cbis-ddsm-calc-training-full-mammogram-images /home/jlandesman/data/train/calc_full
+gcsfuse cbis-ddsm-calc-training-roi-and-cropped-images /home/jlandesman/data/train/calc_cropped
+gcsfuse cbis-ddsm-mass-training-roi-and-cropped-images /home/jlandesman/data/train/mass_cropped
+gcsfuse cbis-ddsm-mass-training-full-mammogram-images /home/jlandesman/data/train/mass_full
 
 gcsfuse cbis-ddsm-calc-test-full-mammogram-images /home/jlandesman/data/test/calc_full
 gcsfuse cbis-ddsm-calc-test-roi-and-cropped-images /home/jlandesman/data/test/calc_cropped
