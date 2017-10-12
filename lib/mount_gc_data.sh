0688fae82e3863753f9ae551b379539211d58e76
jlandesman <jlandesman@users.noreply.github.com>
2017-10-11 20:17:21 -0400

    removed /home/jlandesman from dir structure

--- lib/mount_gc_data.sh
+++ lib/mount_gc_data.sh
@@ -16,15 +16,15 @@ mkdir data/test/calc_cropped
 mkdir data/mias_mini
 chmod -R 777 data
 
-gcsfuse cbis-ddsm-calc-training-full-mammogram-images /home/jlandesman/data/train/calc_full
-gcsfuse cbis-ddsm-calc-training-roi-and-cropped-images /home/jlandesman/data/train/calc_cropped
-gcsfuse cbis-ddsm-mass-training-roi-and-cropped-images /home/jlandesman/data/train/mass_cropped
-gcsfuse cbis-ddsm-mass-training-full-mammogram-images /home/jlandesman/data/train/mass_full
+gcsfuse cbis-ddsm-calc-training-full-mammogram-images data/train/calc_full
+gcsfuse cbis-ddsm-calc-training-roi-and-cropped-images data/train/calc_cropped
+gcsfuse cbis-ddsm-mass-training-roi-and-cropped-images data/train/mass_cropped
+gcsfuse cbis-ddsm-mass-training-full-mammogram-images data/train/mass_full
 
-gcsfuse cbis-ddsm-calc-test-full-mammogram-images /home/jlandesman/data/test/calc_full
-gcsfuse cbis-ddsm-calc-test-roi-and-cropped-images /home/jlandesman/data/test/calc_cropped
-gcsfuse cbis-ddsm-mass-test-roi-and-cropped-images /home/jlandesman/data/test/mass_cropped
-gcsfuse cbis-ddsm-mass-test-full-mammogram-images /home/jlandesman/data/test/mass_full
+gcsfuse cbis-ddsm-calc-test-full-mammogram-images data/test/calc_full
+gcsfuse cbis-ddsm-calc-test-roi-and-cropped-images data/test/calc_cropped
+gcsfuse cbis-ddsm-mass-test-roi-and-cropped-images data/test/mass_cropped
+gcsfuse cbis-ddsm-mass-test-full-mammogram-images data/test/mass_full
 
 gcsfuse mias-mini-tiff /home/jlandesman/data/mias_mini
 
