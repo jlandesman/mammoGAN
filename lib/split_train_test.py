4dd700cfcf34300dd2b31125c2c2a0053b5c1b0b
agunapal <agunapal@ischool.berkeley.edu>
2017-10-13 18:13:55 +0000

    edit

--- lib/split_train_test.py
+++ lib/split_train_test.py
@@ -22,6 +22,7 @@ severity = mias_mini[mias_mini['abnormaility_class']=='CALC']['severity'].value_
 mias_mini_train = pd.DataFrame([])
 mias_mini_test = pd.DataFrame([])
 
+'''
 for item in abnormality:
     abClass = mias_mini[mias_mini['abnormaility_class'] == item]
     if item == "NORM": 
@@ -34,48 +35,62 @@ for item in abnormality:
             train_count = int(round(sevClass.shape[0] * 0.8))
             mias_mini_train = mias_mini_train.append(sevClass.head(train_count))
             mias_mini_test = mias_mini_test.append(sevClass.tail(sevClass.shape[0] - train_count))
-            
 mias_mini_trainSel = mias_mini_train[['ref_num','severity']].copy()
 mias_mini_testSel = mias_mini_test[['ref_num','severity']].copy()            
+'''
+            
+mias_mini_trainSel = mias_mini[['ref_num','severity']].copy()
+#mias_mini_testSel = mias_mini[['ref_num','severity']].copy()            
 
 mias_mini_trainSel = mias_mini_trainSel.fillna(0)
 convert = {"severity": {'B': 1, 'M': 2 }}
 mias_mini_trainSel.replace(convert, inplace=True)
-mias_mini_testSel = mias_mini_testSel.fillna(0)
-mias_mini_testSel.replace(convert, inplace=True)
+
+severity = mias_mini_trainSel['severity'].value_counts().keys()
+#mias_mini_testSel = mias_mini_testSel.fillna(0)
+#mias_mini_testSel.replace(convert, inplace=True)
 
 if os.path.exists(TRAIN_DIR):
     os.system('rm -rf ' + TRAIN_DIR)
 os.system('mkdir ' + TRAIN_DIR)
 os.system('chmod +wx ' + TRAIN_DIR)
-if os.path.exists(TEST_DIR):
-    os.system('rm -rf ' + TEST_DIR)
-os.system('mkdir '+ TEST_DIR)
-os.system('chmod +wx '  + TEST_DIR)
+
+for sev in severity:
+    print sev, TRAIN_DIR
+    os.system('mkdir ' + TRAIN_DIR + '/' + str(sev))
+    os.system('chmod +wx ' + TRAIN_DIR + '/' + str(sev))
+#if os.path.exists(TEST_DIR):
+#    os.system('rm -rf ' + TEST_DIR)
+#os.system('mkdir '+ TEST_DIR)
+#os.system('chmod +wx '  + TEST_DIR)
 
 for name in mias_mini_trainSel['ref_num']:
     #print name
 
-    opFile = TRAIN_DIR + '/' + name +  '.' + FILE_TYPE
+    classLabel = mias_mini_trainSel[mias_mini_trainSel['ref_num'] == name]['severity'].item()
+
+    #print classLabel
+    opFile = TRAIN_DIR + '/' + str(classLabel) + '/' + name +  '.' + FILE_TYPE
     ipFile =  IP_DIR + '/' +name + '.' + FILE_TYPE
-    
+    #print opFile 
     if os.path.isfile(ipFile):
         os.system('cp ' + ipFile + ' ' +opFile)
         
+'''
 for name in mias_mini_testSel['ref_num']:
     #print name
 
-    opFile = TEST_DIR + '/' + name + '.' + FILE_TYPE
+    opFile = TEST_DIR + '/' + str(classLabel) + '/' +name + '.' + FILE_TYPE
     ipFile = IP_DIR + '/' + name + '.' + FILE_TYPE
     
     if os.path.isfile(ipFile):
         os.system('cp ' + ipFile + ' ' +opFile)
-        
+'''        
 print "training files are in the directory ", TRAIN_DIR 
-print "test files are in the directory ", TEST_DIR
+#print "test files are in the directory ", TEST_DIR
 
-mias_mini_trainSel['severity'].to_csv("train_labels.csv",index=False)
-mias_mini_testSel['severity'].to_csv("test_labels.csv",index=False)
+#mias_mini_trainSel['severity'].to_csv("train_labels.csv",index=False)
+#mias_mini_testSel['severity'].to_csv("test_labels.csv",index=False)
 print
-print "Train labels in train_labels.csv"
-print "Test labels in test_labels.csv"
+#print "Train labels in train_labels.csv"
+#print "Test labels in test_labels.csv"
