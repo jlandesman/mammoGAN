f0c984cb2780c5054c604742290556e6b7c9b26b
jlandesman <jlandesman@users.noreply.github.com>
2017-11-08 14:34:35 -0500

    mount all data in png format

---
+++ lib/mount_png_data.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+sudo rm -rf data_png/
+mkdir data_png
+mkdir data_png/mass_cropped
+mkdir data_png/mass_full
+
+mkdir data_png/calc_full
+mkdir data_png/calc_cropped
+
+mkdir data_png/mias_mini
+
+chmod -R 777 data_png
+
+gcsfuse cbis-ddsm-calc-full-mammogram-images data_png/calc_full
+gcsfuse cbis-ddsm-calc-roi-and-cropped-images data_png/calc_cropped
+gcsfuse cbis-ddsm-mass-roi-and-cropped-images data_png/mass_cropped
+gcsfuse cbis-ddsm-mass-full-mammogram-images data_png/mass_full
+gcsfuse mias-mini-png data/mias_mini
