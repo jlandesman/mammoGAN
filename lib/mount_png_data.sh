ade6445cd89a45c0a613004378f3aec47d48de10
jlandesman <jlandesman@users.noreply.github.com>
2017-11-08 14:46:20 -0500

    input to match typo in c

--- lib/mount_png_data.sh
+++ lib/mount_png_data.sh
@@ -13,6 +13,6 @@ chmod -R 777 data_png
 
 gcsfuse cbis-ddsm-calc-full-mammogram-images data_png/calc_full
 gcsfuse cbis-ddsm-calc-roi-and-cropped-images data_png/calc_cropped
-gcsfuse cbis-ddsm-mass-roi-and-cropped-images data_png/mass_cropped
+gcsfuse cbis-ddsm-mass-roi-and-croppsed-images data_png/mass_cropped
 gcsfuse cbis-ddsm-mass-full-mammogram-images data_png/mass_full
-gcsfuse mias-mini-png data/mias_mini
+gcsfuse mias-mini-png data_png/mias_mini
