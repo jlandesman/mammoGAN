3c77913ada9d6d2655ace314e4ef1b061b4ca5a1
jlandesman <jlandesman@users.noreply.github.com>
2017-11-10 14:19:00 -0500

    adding --implicit-dirs to call

--- lib/mount_png_data.sh
+++ lib/mount_png_data.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 sudo rm -rf data_png/
 mkdir data_png
+
 mkdir data_png/mass_cropped
 mkdir data_png/mass_full
 
@@ -8,11 +9,13 @@ mkdir data_png/calc_full
 mkdir data_png/calc_cropped
 
 mkdir data_png/mias_mini
+mkdir data_png/mias_mini_balanced
 
 chmod -R 777 data_png
 
-gcsfuse cbis-ddsm-calc-full-mammogram-images data_png/calc_full
-gcsfuse cbis-ddsm-calc-roi-and-cropped-images data_png/calc_cropped
-gcsfuse cbis-ddsm-mass-roi-and-croppsed-images data_png/mass_cropped
-gcsfuse cbis-ddsm-mass-full-mammogram-images data_png/mass_full
-gcsfuse mias-mini-png data_png/mias_mini
+gcsfuse --implicit-dirs cbis-ddsm-calc-full-mammogram-images data_png/calc_full
+gcsfuse --implicit-dirs cbis-ddsm-calc-roi-and-cropped-images data_png/calc_cropped
+gcsfuse --implicit-dirs cbis-ddsm-mass-roi-and-croppsed-images data_png/mass_cropped
+gcsfuse --implicit-dirs cbis-ddsm-mass-full-mammogram-images data_png/mass_full
+gcsfuse --implicit-dirs mias-mini-png data_png/mias_mini
+gcsfuse --implicit-dirs mias-mini-balanced data_png/mias-mini-balanced
